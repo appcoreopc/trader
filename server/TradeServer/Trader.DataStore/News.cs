40fb232d617080c35aee494642c0ca042abfb43e
jeremy <kepung@gmail.com>
2017-06-01 22:13:34 +1200

    integrated company news with graph ql

---
+++ server/TradeServer/Trader.DataStore/News.cs
@@ -0,0 +1,12 @@
+﻿namespace Trader.DataStore
+{
+    public class News
+    {
+        public string Id { get; set; }
+
+        public string SecurityCode { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
---
+++ server/TradeServer/Trader.DataStore/QueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System.Threading.Tasks;
+
+namespace Trader.DataStore
+{
+    class QueryHandler
+    {
+        public async Task ExecuteQuery(GraphQLQuery query)
+        {
+           var result = await new DocumentExecuter().ExecuteAsync(x =>
+           {
+            //schema 
+             x.Query = query.Query;
+           }).ConfigureAwait(false);
+        }
+    }
+    
+    public class CompanyStockData : ObjectGraphType
+    {
+        public CompanyStockData()
+        {
+            Field<NewsType>("news", resolve: context => new News()); // Get news streams
+
+            Field<NewsType>("new", resolve: context => new News()); // Get news streams
+        }
+    }
+
+    public class NewsType : ObjectGraphType<News>
+    {
+        public NewsType()
+        {
+            Field(x => x.Id).Description("Unique id for the news");
+            Field(x => x.SecurityCode).Description("Security code");
+            Field(x => x.Description).Description("News info");
+        }
+    }
+}
